0f9386477329b9889a55278459d44fe4fae13a7a
Pranav Padawe <pranavpadave501@gmail.com>
2021-08-15 21:37:41 +0530

    login register

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
 //database connections
 
@@ -20,6 +21,10 @@ connection.once('open', ()=>{
 }).catch(err =>{
   console.log('Connection failed......')
 });
+
+
+
+
 
 //session store
 /*let mongoStore = new MongoDbStore({
@@ -39,14 +44,23 @@ app.use(session({
   cookie: { maxAge: 100*60*60*24 }       //24 hrs in ms
 }))
 
+//passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash());
 //Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 
 //Global middleware
 app.use((req, res, next)=>{
   res.locals.session = req.session
+  res.locals.user = req.user
   next()
 })
 
